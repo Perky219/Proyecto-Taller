ebe0d8a18b766c49f6cfdce5db19ebbb50d03c60
Perky219 <perky6929@gmail.com>
2023-05-15 10:28:57 -0600

    Código final antes de interfaz

--- Avance_Proyecto.py
+++ Avance_Proyecto.py
@@ -214,205 +214,190 @@ def speech(participantes, agenda):
                 i += 1
     print("Fin del reconocimiento de voz.")
     return text_cap
-
-def primer_reporte():
-    """
-    Esta función imprime la lista de participantes, la agenda completa y las transcripciones de cada participante.
-
-    Args:
-        participante (str): Condicional de un ciclo for que toma las credenciales de cada participante para imprimir.
-        puntos (tuple): Condicional de un ciclo for que toma el valor de cada sección de la agenda.
-        punto (str): Condicional de ciclo for anidado que toma el valor de un punto específico de cada sección de la agenda.
-        i (list): Toma cada sublista como condicional de un ciclo for, para poder hacer una impresión.
-    """
-    # Se imprime la lista de participantes.
-    print("\nLista de participantes:")
-    for participante in participantes:
-        print(participante["nombre"] + " - " + participante["carnet"])
-
-    # Se imprime la agenda completa.
-    print("\nAgenda:")
-    for apartado, puntos in agenda:
-        print(apartado)
-        for punto in puntos:
-            print(" - " + punto+'\n')
-
-    # Imprimir transcripciones de cada participante
-    for i in text_cap:
-        print(f"{i[0]['nombre']} en el punto '{i[1][1]}' del apartado '{i[1][0]}' a las {i[2]} dijo: '{i[3][0]}'\n")
-
-def segundo_reporte():
-    """
-    Esta función cuenta la cantidad de palabras reconocidas por participante y las imprime en orden descendente.
-
-    Args:
-        palabras_por_participante (dict): Diccionario que contendrá las palabras que dice cada participante.
-        transcripcion (list): Toma el valor de las sublistas dentro de "text_cap".
-        participante (str): Toma el nombre de cada participante contenido en "text_cap".
-        palabras (int): Valor numérico del total de palabras.
-        ordenado (dict): Contiene las personas ordenadas por cantidad de palabras.
-        cantidad_palabras (int): Condicional del ciclo for que toma el valor de la cantidad de palabras de cada participante dentro de "ordenado".
-    """
-    # Diccionario para almacenar la cantidad de palabras reconocidas de cada participante.
-    palabras_por_participante = {}
-
-    # Recorremos las transcripciones y contamos las palabras de cada participante.
-    for transcripcion in text_cap:
-        participante = transcripcion[0]["nombre"]
-        palabras = len(transcripcion[3][0].split())
-        if participante in palabras_por_participante:
-            palabras_por_participante[participante] += palabras
-        else:
-            palabras_por_participante[participante] = palabras
-
-    # Ordenamos el diccionario en base a la cantidad de palabras reconocidas.
-    ordenado = sorted(palabras_por_participante.items(), key=lambda x: x[1], reverse=True)
-
-    # Imprimimos el resultado.
-    print("\nCantidad de palabras reconocidas por participante:")
-    for participante, cantidad_palabras in ordenado:
-        print(f"{participante}: {cantidad_palabras} palabras")
-
-def tercer_reporte():
-    """
-    Esta función cuenta la cantidad total de participaciones por persona y por punto, y también imprime el listado de personas que participaron por punto.
-
-    Args:
-        participaciones_por_punto (dict): Diccionario que registra las participaciones por punto de cada persona.
-        personas_por_punto (dict): Diccionario que registra las personas que participaron por punto.
-        transcripcion (list): Recorre la lista "text_cap" y toma el valor de las listas dentro de esta.
-        nombre (str): Valor de nombre que se toma dentro de las sublistas en "text_cap".
-        punto (str): Valor de punto que se toma dentro de las sublistas en "text_cap".
-        participo (bool): Condicional que toma valor inicial en False para realizar comparaciones.
-    """
-    # Diccionario para la cantidad de participaciones por persona y por punto.
-    participaciones_por_punto = {}
-
-    # Diccionario para el listado de personas que participaron por punto.
-    personas_por_punto = {}
-
-    # Recorrer las transcripciones.
-    for transcripcion in text_cap:
-        nombre = transcripcion[0]["nombre"]
-        punto = transcripcion[1]
-        
-        # Actualizar la cantidad de participaciones por persona y por punto.
-        if nombre not in participaciones_por_punto:
-            participaciones_por_punto[nombre] = {}
-        if punto not in participaciones_por_punto[nombre]:
-            participaciones_por_punto[nombre][punto] = 0
-        participaciones_por_punto[nombre][punto] += 1
-        
-        # Actualizar el listado de personas que participaron por punto.
-        if punto not in personas_por_punto:
-            personas_por_punto[punto] = []
-        participo = False
-        for i, tupla in enumerate(personas_por_punto[punto]):
-            if tupla[0] == nombre:
-                personas_por_punto[punto][i] = (nombre, tupla[1] + 1)
-                participo = True
-                break
-        if not participo:
-            personas_por_punto[punto].append((nombre, 1))
-
-    # Imprimir la cantidad total de participaciones por persona y por punto.
-    print("\nCantidad total de participaciones por persona y por punto:")
-    for nombre, participaciones in participaciones_por_punto.items():
-        print(f"\n{nombre}:")
-        for punto, cantidad in participaciones.items():
-            print(f"  - En el apartado {punto[0]}, punto {punto[1]}: {cantidad}")
-
-    # Imprimir el listado de personas que participaron por punto.
-    print("\nListado de personas que participaron por punto:")
-    for punto, personas in personas_por_punto.items():
-        print(f"\nApartado {punto[0]}, punto {punto[1]}:")
-        for persona in personas:
-            print(f"  - {persona[0]}: {persona[1]} veces")
-
-# # Crear la ventana
-# ventana = tk.Tk()
-# ventana.title("Mi Ventana")
-
-# # Crear el botón para definir la agenda
-# btn_agenda = tk.Button(ventana, text="Definir agenda", command=registro_agenda)
-# btn_agenda.pack(padx=10, pady=10)
-
-# # Crear el botón para ingresar la lista de participantes
-# btn_participantes = tk.Button(ventana, text="Lista de participantes", command=lista_participantes)
-# btn_participantes.pack(padx=10, pady=10)
-
-# # Mostrar la ventana
-# ventana.mainloop()
 #---------------------------------------------------------------------------------------------------#
-def menu_reportes():
-    """
-    Función que muestra un menú para seleccionar uno de los tres reportes disponibles y lo genera.
+# def primer_reporte():
+#     """
+#     Esta función imprime la lista de participantes, la agenda completa y las transcripciones de cada participante.
 
-    Attributes:
-        opcion (str): Opción seleccionada por el usuario.
-    """
-    while True:
-        print("\n--- MENÚ DE REPORTES ---")
-        print("1. Reporte de participaciones por apartado de la agenda")
-        print("2. Reporte de cantidad total de palabras reconocidas por persona")
-        print("3. Reporte de cantidad total de participaciones por persona y por punto de la agenda")
-        print("4. Salir")
-        opcion = input("Seleccione el número del reporte que desea generar: ")
+#     Args:
+#         participante (str): Condicional de un ciclo for que toma las credenciales de cada participante para imprimir.
+#         puntos (tuple): Condicional de un ciclo for que toma el valor de cada sección de la agenda.
+#         punto (str): Condicional de ciclo for anidado que toma el valor de un punto específico de cada sección de la agenda.
+#         i (list): Toma cada sublista como condicional de un ciclo for, para poder hacer una impresión.
+#     """
+#     # Se imprime la lista de participantes.
+#     print("\nLista de participantes:")
+#     for participante in participantes:
+#         print(participante["nombre"] + " - " + participante["carnet"])
+
+#     # Se imprime la agenda completa.
+#     print("\nAgenda:")
+#     for apartado, puntos in agenda:
+#         print(apartado)
+#         for punto in puntos:
+#             print(" - " + punto+'\n')
+
+#     # Imprimir transcripciones de cada participante
+#     for i in text_cap:
+#         print(f"{i[0]['nombre']} en el punto '{i[1][1]}' del apartado '{i[1][0]}' a las {i[2]} dijo: '{i[3][0]}'\n")
+
+# def segundo_reporte():
+#     """
+#     Esta función cuenta la cantidad de palabras reconocidas por participante y las imprime en orden descendente.
+
+#     Args:
+#         palabras_por_participante (dict): Diccionario que contendrá las palabras que dice cada participante.
+#         transcripcion (list): Toma el valor de las sublistas dentro de "text_cap".
+#         participante (str): Toma el nombre de cada participante contenido en "text_cap".
+#         palabras (int): Valor numérico del total de palabras.
+#         ordenado (dict): Contiene las personas ordenadas por cantidad de palabras.
+#         cantidad_palabras (int): Condicional del ciclo for que toma el valor de la cantidad de palabras de cada participante dentro de "ordenado".
+#     """
+#     # Diccionario para almacenar la cantidad de palabras reconocidas de cada participante.
+#     palabras_por_participante = {}
+
+#     # Recorremos las transcripciones y contamos las palabras de cada participante.
+#     for transcripcion in text_cap:
+#         participante = transcripcion[0]["nombre"]
+#         palabras = len(transcripcion[3][0].split())
+#         if participante in palabras_por_participante:
+#             palabras_por_participante[participante] += palabras
+#         else:
+#             palabras_por_participante[participante] = palabras
+
+#     # Ordenamos el diccionario en base a la cantidad de palabras reconocidas.
+#     ordenado = sorted(palabras_por_participante.items(), key=lambda x: x[1], reverse=True)
+
+#     # Imprimimos el resultado.
+#     print("\nCantidad de palabras reconocidas por participante:")
+#     for participante, cantidad_palabras in ordenado:
+#         print(f"{participante}: {cantidad_palabras} palabras")
+
+# def tercer_reporte():
+#     """
+#     Esta función cuenta la cantidad total de participaciones por persona y por punto, y también imprime el listado de personas que participaron por punto.
+
+#     Args:
+#         participaciones_por_punto (dict): Diccionario que registra las participaciones por punto de cada persona.
+#         personas_por_punto (dict): Diccionario que registra las personas que participaron por punto.
+#         transcripcion (list): Recorre la lista "text_cap" y toma el valor de las listas dentro de esta.
+#         nombre (str): Valor de nombre que se toma dentro de las sublistas en "text_cap".
+#         punto (str): Valor de punto que se toma dentro de las sublistas en "text_cap".
+#         participo (bool): Condicional que toma valor inicial en False para realizar comparaciones.
+#     """
+#     # Diccionario para la cantidad de participaciones por persona y por punto.
+#     participaciones_por_punto = {}
+
+#     # Diccionario para el listado de personas que participaron por punto.
+#     personas_por_punto = {}
+
+#     # Recorrer las transcripciones.
+#     for transcripcion in text_cap:
+#         nombre = transcripcion[0]["nombre"]
+#         punto = transcripcion[1]
         
-        if opcion == "1":
-            os.system('clear')
-            primer_reporte()
-        elif opcion == "2":
-            os.system('clear')
-            segundo_reporte()
-        elif opcion == "3":
-            os.system('clear')
-            tercer_reporte()
-        elif opcion == "4":
-            os.system('clear')
-            print("¡Hasta luego!")
-            time.sleep(3)
-            os.system('clear')
-            break
-        else:
-            print("Opción inválida, por favor intente de nuevo.")
+#         # Actualizar la cantidad de participaciones por persona y por punto.
+#         if nombre not in participaciones_por_punto:
+#             participaciones_por_punto[nombre] = {}
+#         if punto not in participaciones_por_punto[nombre]:
+#             participaciones_por_punto[nombre][punto] = 0
+#         participaciones_por_punto[nombre][punto] += 1
+        
+#         # Actualizar el listado de personas que participaron por punto.
+#         if punto not in personas_por_punto:
+#             personas_por_punto[punto] = []
+#         participo = False
+#         for i, tupla in enumerate(personas_por_punto[punto]):
+#             if tupla[0] == nombre:
+#                 personas_por_punto[punto][i] = (nombre, tupla[1] + 1)
+#                 participo = True
+#                 break
+#         if not participo:
+#             personas_por_punto[punto].append((nombre, 1))
 
-def menu_principal():
-    """
-    Función que genera un menú en terminal, en dónde el usuario puede seleccionar distintas opciones para ejecutar lo que prefiera.
+#     # Imprimir la cantidad total de participaciones por persona y por punto.
+#     print("\nCantidad total de participaciones por persona y por punto:")
+#     for nombre, participaciones in participaciones_por_punto.items():
+#         print(f"\n{nombre}:")
+#         for punto, cantidad in participaciones.items():
+#             print(f"  - En el apartado {punto[0]}, punto {punto[1]}: {cantidad}")
 
-    Attributes:
-        opcion (str): Opción seleccionada por el usuario.
-    """
-    while True:
-        os.system('clear')
-        print("Menú:")
-        print("1. Registrar participantes")
-        print("2. Definir agenda")
-        print("3. Reconocimiento de voz")
-        print("4. Generar reportes")
-        print("5. Salir")
-        opcion = input("Ingrese la opción deseada: ")
+#     # Imprimir el listado de personas que participaron por punto.
+#     print("\nListado de personas que participaron por punto:")
+#     for punto, personas in personas_por_punto.items():
+#         print(f"\nApartado {punto[0]}, punto {punto[1]}:")
+#         for persona in personas:
+#             print(f"  - {persona[0]}: {persona[1]} veces")
 
-        if opcion == "1":
-            os.system('clear')
-            lista_participantes()
-        elif opcion == "2":
-            os.system('clear')
-            registro_agenda()
-        elif opcion == "3":
-            os.system('clear')
-            speech(participantes, agenda)
-        elif opcion == "4":
-            os.system('clear')
-            menu_reportes()
-        elif opcion=="5":
-            os.system('clear')
-            print("¡Gracias por utilizar nuestro programa!")
-            time.sleep(3)
-            os.system('clear')
-            break
-        else:
-            print("Opción no válida.")
+# def menu_reportes():
+#     """
+#     Función que muestra un menú para seleccionar uno de los tres reportes disponibles y lo genera.
 
-menu_principal()
+#     Attributes:
+#         opcion (str): Opción seleccionada por el usuario.
+#     """
+#     while True:
+#         print("\n--- MENÚ DE REPORTES ---")
+#         print("1. Reporte de participaciones por apartado de la agenda")
+#         print("2. Reporte de cantidad total de palabras reconocidas por persona")
+#         print("3. Reporte de cantidad total de participaciones por persona y por punto de la agenda")
+#         print("4. Salir")
+#         opcion = input("Seleccione el número del reporte que desea generar: ")
+        
+#         if opcion == "1":
+#             os.system('clear')
+#             primer_reporte()
+#         elif opcion == "2":
+#             os.system('clear')
+#             segundo_reporte()
+#         elif opcion == "3":
+#             os.system('clear')
+#             tercer_reporte()
+#         elif opcion == "4":
+#             os.system('clear')
+#             print("¡Hasta luego!")
+#             time.sleep(3)
+#             os.system('clear')
+#             break
+#         else:
+#             print("Opción inválida, por favor intente de nuevo.")
+
+# def menu_principal():
+#     """
+#     Función que genera un menú en terminal, en dónde el usuario puede seleccionar distintas opciones para ejecutar lo que prefiera.
+
+#     Attributes:
+#         opcion (str): Opción seleccionada por el usuario.
+#     """
+#     while True:
+#         os.system('clear')
+#         print("Menú:")
+#         print("1. Registrar participantes")
+#         print("2. Definir agenda")
+#         print("3. Reconocimiento de voz")
+#         print("4. Generar reportes")
+#         print("5. Salir")
+#         opcion = input("Ingrese la opción deseada: ")
+
+#         if opcion == "1":
+#             os.system('clear')
+#             lista_participantes()
+#         elif opcion == "2":
+#             os.system('clear')
+#             registro_agenda()
+#         elif opcion == "3":
+#             os.system('clear')
+#             speech(participantes, agenda)
+#         elif opcion == "4":
+#             os.system('clear')
+#             menu_reportes()
+#         elif opcion=="5":
+#             os.system('clear')
+#             print("¡Gracias por utilizar nuestro programa!")
+#             time.sleep(3)
+#             os.system('clear')
+#             break
+#         else:
+#             print("Opción no válida.")
+
+# menu_principal()
