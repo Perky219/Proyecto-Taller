c8ae993149d8938991e647692c79aecb1b29c220
Perky219 <perky6929@gmail.com>
2023-06-08 23:25:18 -0600

    primeros avances de interfaz

--- Entrega3/InterfazAgenda.py
+++ Entrega3/InterfazAgenda.py
@@ -1,62 +1,37 @@
-from customtkinter import Tk, Label, Entry, Button, Listbox
-import LogicaAgenda
+from tkinter import Tk, Label, Entry, Button
+import datetime
 
 class AgendaInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Agenda Web")
         
-        self.agenda = LogicaAgenda.Agenda("", "", 3)
+        # Obtener la fecha actual
+        fecha_actual = datetime.date.today()
         
-        # Crear etiquetas y campos de entrada para el registro de agenda
+        # Crear etiqueta y campo de entrada para el título
         self.label_title = Label(self.window, text="Título:")
         self.entry_title = Entry(self.window)
+        
+        # Crear etiqueta para la fecha de creación
         self.label_date = Label(self.window, text="Fecha de creación:")
-        self.entry_date = Entry(self.window)
+        self.label_date_value = Label(self.window, text=str(fecha_actual))
         
-        # Crear lista para mostrar participantes
-        self.participants_listbox = Listbox(self.window)
+        # Crear botón para guardar el título
+        self.button_save_title = Button(self.window, text="Guardar título", command=self.save_title)
         
-        # Crear botones para agregar participantes y eliminar participantes seleccionados
-        self.button_add_participant = Button(self.window, text="Agregar Participante", command=self.add_participant)
-        self.button_remove_participant = Button(self.window, text="Eliminar Participante", command=self.remove_participant)
-        
-        # Otros elementos de la interfaz...
-        
-        # Configurar el posicionamiento de los elementos en la ventana
-        self.label_title.pack()
-        self.entry_title.pack()
-        self.label_date.pack()
-        self.entry_date.pack()
-        self.participants_listbox.pack()
-        self.button_add_participant.pack()
-        self.button_remove_participant.pack()
+        # Posicionamiento de los elementos en la ventana usando grid
+        self.label_title.grid(row=0, column=0, sticky="e")
+        self.entry_title.grid(row=0, column=1, padx=5, pady=5)
+        self.label_date.grid(row=1, column=0, sticky="e")
+        self.label_date_value.grid(row=1, column=1, padx=5, pady=5)
+        self.button_save_title.grid(row=2, columnspan=2, padx=5, pady=5)
         
         self.window.mainloop()
     
-    def add_participant(self):
-        participant_name = self.entry_name.get()
-        participant_last_name1 = self.entry_last_name1.get()
-        participant_last_name2 = self.entry_last_name2.get()
+    def save_title(self):
+        title = self.entry_title.get()
+        self.entry_title.config(state="disabled")
+        self.button_save_title.config(state="disabled")
         
-        participant = LogicaAgenda.Participante(participant_name, participant_last_name1, participant_last_name2)
-        
-        self.agenda.agregar_participante(participant)
-        
-        self.update_participants_listbox()
-        
-    def remove_participant(self):
-        selected_participant = self.participants_listbox.get(self.participants_listbox.curselection())
-        
-        for participant in self.agenda.participantes:
-            if participant.nombre == selected_participant:
-                self.agenda.eliminar_participante(participant)
-                break
-        
-        self.update_participants_listbox()
-        
-    def update_participants_listbox(self):
-        self.participants_listbox.delete(0, "end")
-        
-        for participant in self.agenda.participantes:
-            self.participants_listbox.insert("end", participant.nombre)
+AgendaInterface()
